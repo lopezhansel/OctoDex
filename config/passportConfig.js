b6f4069597b70c065309fed0e0de56a93d11d4cf
Hansel <lopezhansel@gmail.com>
2015-11-28 21:11:49 -0700

    temporarily changed const to var. Now saving new users when logging in with github

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,37 +1,52 @@
-const GitHubStrategy = require('passport-github2').Strategy;
-console.log(GitHubStrategy);
+var GitHubStrategy = require('passport-github2').Strategy;
+var oauth = require("./oauth");
+var User = require("../models/userModel");
 
-const oauth = require("./oauth");
-
-const User = require("../models/userModel");
-
-
-
-module.exports = function (passport) {
+module.exports = function(passport) {
 
 	passport.serializeUser(function(user, done) {
-	  done(null, user);
+		done(null, user);
 	});
 
-	passport.deserializeUser(function(obj, done) {
-	  done(null, obj);
+	passport.deserializeUser(function(user, done) {
+		User.findById(user._id, function(err, user) {
+			done(err, user);
+		});
 	});
 
 	passport.use(new GitHubStrategy({
-	    clientID: oauth.github.clientID,
-	    clientSecret: oauth.github.clientSecret,
-	    callbackURL: oauth.github.callbackURL
-	  },
-	  function(accessToken, refreshToken, profile, done) {
-	    console.log(profile);
+			clientID: oauth.github.clientID,
+			clientSecret: oauth.github.clientSecret,
+			callbackURL: oauth.github.callbackURL
+		},
+		function(accessToken, refreshToken, profile, done) {
+			process.nextTick(function() {
+				User.findOne({
+					'id': profile.id
+				}, function(err, user) {
+					if (err)
+						return (err);
+					if (user)
+						return done(null, user);
+					else {
+						var newUser = new User();
 
-	    // User.findOrCreate({
-	    //  githubId: profile.id
-	    // }, function(err, user) {
-	    //  console.log(user);
-	    //  return done(err, user);
-	    // });
-	  }
+						newUser.id = profile.id;
+						newUser.token = accessToken;
+						newUser.name = profile._json.name;
+						newUser.email = profile._json.email;
+						newUser.username = profile.username;
+
+						newUser.save(function(err) {
+							if (err)
+								throw err;
+							return done(null, newUser);
+						});
+					}
+				});
+			});
+		}
+
 	));
-	
+
 };
