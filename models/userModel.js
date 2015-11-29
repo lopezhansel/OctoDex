457beb96e30c134a5a1de13598f7f1eeb791287b
Hansel <lopezhansel@gmail.com>
2015-11-28 22:14:28 -0700

    Changed schema for clarity

--- models/userModel.js
+++ models/userModel.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
 
     username     : {type: String, required: true},
-    token        : {type: String, required: true},
     name         : {type: String, required: true},
     email        : {type: String, required: true},
+    gitToken     : {type: String, required: true},
     githubId     : {type: String, required: true}
 
 });
