aa59ed792419f79054711c1091fbb64ba739e50c
Hansel <lopezhansel@gmail.com>
2015-12-07 19:46:51 -0700

    Adding files for other users profiles

---
+++ public/js/otherProfileController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.controller('otherProfileController', ["$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "gamService", function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, gamService) {
+	alert("hello");
+}]);
---
+++ src/js/otherProfileController.js
@@ -0,0 +1,4 @@
+app.controller('otherProfileController', [  "$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "gamService",function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, gamService) {
+	alert("hello");
+	
+}]);
