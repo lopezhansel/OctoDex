d3902fd38eb2fafd69468932e14bcb11feb04156
Hansel <lopezhansel@gmail.com>
2016-01-12 23:44:18 -0700

    Enabling iewFullProfile button

--- public/js/myProfileController.js
+++ public/js/myProfileController.js
@@ -4,6 +4,7 @@ app.controller('myProfileController', ["$scope", '$routeParams', '$mdMedia', '$m
 
 	$scope.octo = octo;
 	$scope.user = octo.me;
+	$scope.viewFullProfile = false;
 
 	$timeout(function () {
 		$scope.octo = octo;
