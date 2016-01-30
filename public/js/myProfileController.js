9f011afdceeee71c57718a67f14a27e899db9f5e
Hansel <lopezhansel@gmail.com>
2016-01-30 12:47:20 -0700

    Cleaning up myProfileController

--- public/js/myProfileController.js
+++ public/js/myProfileController.js
@@ -1,17 +1,23 @@
 'use strict';
 
 app.controller('myProfileController', ["$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "octoService", function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, octoService) {
+
+	// atHome enables different views
 	$scope.atHome = $location.path() === "/" ? true : false;
 	$scope.octoService = octoService;
-	$scope.user = octoService.client;
+	$scope.fullProfileSwitch = false;
 	$scope.toggleFullProfileSwitch = function () {
 		$scope.fullProfileSwitch = !$scope.fullProfileSwitch;
 	};
-	$scope.fullProfileSwitch = false;
-	$timeout(function () {
-		$scope.octoService = octoService;
-		if (!$scope.octoService.client) {}
-	}, 1000);
 
-	$scope.updateClient = octoService.updateClient;
+	// interval to check if update ajax request came back
+	var updateUserInterval = $interval(function () {
+		var me = octoService.client;
+		// if me exist with a .githubId or .error  properties
+		if (me && (me.githubId || me.error)) {
+			// then set $scope.user;
+			$scope.user = me;
+			$interval.cancel(updateUserInterval);
+		}
+	}, 20);
 }]);
--- src/js/myProfileController.js
+++ src/js/myProfileController.js
@@ -1,18 +1,26 @@
-app.controller('myProfileController', [  "$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "octoService",function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, octoService) {
+app.controller('myProfileController', 
+	[  "$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "octoService",
+	function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, octoService) {
+
+	// atHome enables different views 
 	$scope.atHome = ($location.path() === "/")? true : false;
 	$scope.octoService = octoService;
-	$scope.user = octoService.client;
+	$scope.fullProfileSwitch = false;
 	$scope.toggleFullProfileSwitch = function  () {
 		$scope.fullProfileSwitch = !$scope.fullProfileSwitch;
 	};
-	$scope.fullProfileSwitch = false;
-	$timeout(() => {
-		$scope.octoService = octoService;
-		if (!$scope.octoService.client){	
+	
+	// interval to check if update ajax request came back
+	var updateUserInterval = $interval( () => {
+		var me = octoService.client;
+		// if me exist with a .githubId or .error  properties
+		if (me && (me.githubId  ||  me.error)) {
+			// then set $scope.user;
+			$scope.user = me;
+			$interval.cancel(updateUserInterval);
 		}
-	}, 1000);	
+	}, 20);
 
-	$scope.updateClient = octoService.updateClient;
 
 
 }]);
