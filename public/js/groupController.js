ccba1ebfd91fe41a205ea21e6fba54b93d46c67f
Hansel <lopezhansel@gmail.com>
2016-03-16 23:35:03 -0600

    Removing file not needed anymore

--- public/js/groupController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.controller('groupController', ["$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "octoService", function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, octoService) {
-
-	var group = $routeParams.group; // just an alias
-	octoService.getOrganizations(group); // Fetch profile
-	$scope.selectGroup = group;
-}]).controller('accountController', ["$scope", '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", "octoService", function ($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout, octoService) {}]);
